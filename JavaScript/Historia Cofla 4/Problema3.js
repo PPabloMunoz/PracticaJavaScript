04a7812eedb8687bad385d75c1b07ecf2eeff231
Pablo304 <pablomunoz.0507@gmail.com>
2022-03-11 23:05:13 +0100

    update

--- JavaScript/Historia Cofla 4/Problema3.js
+++ JavaScript/Historia Cofla 4/Problema3.js
@@ -1,36 +1,36 @@
 
-const materias = {
-    fisica: ["pedro", "pablo", "maria", "juan", "david"],
-    mates: ["pedro", "pablo", "maria", "juan"],
-    programacion: ["pedro", "pablo"],
-}
+// const materias = {
+//     fisica: ["pedro", "pablo", "maria", "juan", "david"],
+//     mates: ["pedro", "pablo", "maria", "juan"],
+//     programacion: ["pedro", "pablo"],
+// }
 
-let materia = prompt("¿donde te quieres inscribir? 1: fisica, 2: mates, 3: programación")
-if (materia == 1) {
-    materia = "fisica"
-}else if (materia == 2) {
-    materia = "mates"
-} else if (materia == 3) {
-    materia = "programacion"
-}else {
-    alert("no se ha encontrado la materia")
-}
+// let materia = prompt("¿donde te quieres inscribir? 1: fisica, 2: mates, 3: programación")
+// if (materia == 1) {
+//     materia = "fisica"
+// }else if (materia == 2) {
+//     materia = "mates"
+// } else if (materia == 3) {
+//     materia = "programacion"
+// }else {
+//     alert("no se ha encontrado la materia")
+// }
 
-const pregunta = (materia)
-    if (materias[materia].length < 5) {
-        alert(`Inscrito a ${materia}`)
-        materias[materia].push(`<b style="color:blue">Cofla</b>`)
-    } else {
-        alert("la clase esta llena")
-    }
+// const pregunta = (materia)
+//     if (materias[materia].length < 5) {
+//         alert(`Inscrito a ${materia}`)
+//         materias[materia].push(`<b style="color:blue">Cofla</b>`)
+//     } else {
+//         alert("la clase esta llena")
+//     }
 
 
 
-document.write(`Alumnos inscritos en fisica son: <b>${materias["fisica"]}</b> <br>`)
+// document.write(`Alumnos inscritos en fisica son: <b>${materias["fisica"]}</b> <br>`)
 
-document.write(`Alumnos inscritos en mates son: <b>${materias["mates"]}</b> <br>`)
+// document.write(`Alumnos inscritos en mates son: <b>${materias["mates"]}</b> <br>`)
 
-document.write(`Alumnos inscritos en programación son: <b>${materias["programacion"]}</b> <br>`)
+// document.write(`Alumnos inscritos en programación son: <b>${materias["programacion"]}</b> <br>`)
 
 
 
