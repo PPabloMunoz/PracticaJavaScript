b58b6af4319a34337be5e94b9edd84519fd63887
Pablo304 <pablomunoz.0507@gmail.com>
2022-03-11 10:46:02 +0100

    update

--- JavaScript/Historia Cofla 4/calculadora.js
+++ JavaScript/Historia Cofla 4/calculadora.js
@@ -1,1 +1,33 @@
 let operacion = prompt("1: Suma, 2: Resta, 3: Multiplicación, 4: División, 5: Potencia, 6: Raiz Cuadrada, 7: Raiz Cubica")
+
+
+const calculadora 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
